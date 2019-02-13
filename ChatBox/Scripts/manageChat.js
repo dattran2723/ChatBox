1f81f99bd95580ec8ae7b26e9cb0e5b01723a269
Tran Dat <Tran Dat@DESKTOP-SLFVQSH>
2019-02-13 16:07:17 +0700

    unpdate managechat

--- ChatBox/Scripts/manageChat.js
+++ ChatBox/Scripts/manageChat.js
@@ -7,4 +7,27 @@
             $('#name-chat').html(name);
         }
     })
+    function newMessage() {
+        var message = $("textarea").val();
+        if ($.trim(message) == '') {
+            return false;
+        }
+        var code = '<li class="cm"><div class="msg_cotainer" >' + message + '<span class="msg_time">8:40 AM, Today</span></div ></li >';
+        //$('<li class="sent"><img src="http://emilcarlsson.se/assets/mikeross.png" alt="" /><p>' + message + '</p></li>').appendto($('.message ul'));
+        $('textarea').val(null);
+        $('.list-msg').append(code);
+        //$('.contact.active .preview').html('<span>you: </span>' + message);
+        $(".list-msg").animate({ scrolltop: $(document).height() }, "fast");
+    };
+
+    $('.send').click(function () {
+        newMessage();
+    });
+
+    $(window).on('keydown', function (e) {
+        if (e.which == 13) {
+            newMessage();
+            return false;
+        }
+    });
 });
