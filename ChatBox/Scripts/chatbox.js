a0c02c9b76d550a08f24769c448058322fa2b263
long đoàn <doanthanhlong1801@gmail.com>
2019-02-13 10:41:08 +0700

    update giao dien chatbox

---
+++ ChatBox/Scripts/chatbox.js
@@ -0,0 +1,16 @@
+﻿$(function () {
+    $('.chatbox-title').click(function () {
+        $('.chatbox').toggleClass('chatbox-tray');
+    })
+    $('.chatbox-close').click(function () {
+        $('.chatbox').addClass('chatbox-closed');
+    })
+
+    //var chatHub = $.connection.myHub;
+    //$.connection.hub.start().done(function () {
+    //    if ($('#btn-StartChat').click(function () {
+    //        chatHub.server.connected($('#txtEmail').val());
+    //        $('.login').hide();
+    //    }));
+    //})
+})
