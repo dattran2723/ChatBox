a6dd7398b7641026c847013bea175696956fe24a
long đoàn <doanthanhlong1801@gmail.com>
2019-02-13 10:26:57 +0700

    up code

---
+++ ChatBox/Startup.cs
@@ -0,0 +1,35 @@
+﻿using ChatBox.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChatBox.Startup))]
+namespace ChatBox
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            CreateAccountDefault();
+        }
+        public void CreateAccountDefault()
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+            var existUser = userManager.FindByEmail("admin@gmail.com");
+            if (existUser == null)
+            {
+                var user = new ApplicationUser();
+                user.UserName = "admin@gmail.com";
+                user.EmailConfirmed = true;
+                user.PhoneNumberConfirmed = true;
+                user.Email = "admin@gmail.com";
+                string password = "admin123";
+
+                userManager.Create(user, password);
+            }
+        }
+    }
+}
