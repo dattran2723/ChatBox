3637f7e6396cccdca4a2d245c18dbf676e61ec0a
Tran Dat <Tran Dat@DESKTOP-SLFVQSH>
2019-02-13 15:22:53 +0700

    add manage chat

---
+++ ChatBox/Scripts/manageChat.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+    $('.left ul').on('click', 'li', function () {
+        if ($(this).hasClass('active') == false) {
+            $('.left ul').children('li').removeClass('active');
+            $(this).addClass('active');
+            var name = $(this).find('.name').text();
+            $('#name-chat').html(name);
+        }
+    })
+});
