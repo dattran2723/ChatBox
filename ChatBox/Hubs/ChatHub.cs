b292d850cf3869bbf2ffb07378b1086867211a22
Tran Dat <Tran Dat@DESKTOP-SLFVQSH>
2019-02-14 09:05:41 +0700

    update manage

--- ChatBox/Hubs/ChatHub.cs
+++ ChatBox/Hubs/ChatHub.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using ChatBox.DataBinding;
 using ChatBox.Models;
 using Microsoft.AspNet.SignalR;
 
@@ -32,24 +33,20 @@ namespace ChatBox.Hubs
                 item.ConnectionId = id;
                 db.SaveChanges();
             }
-            
+
         }
         /// <summary>
         /// gui tin nhan cho admin
         /// </summary>
         public void SendMsg(string fromEmail, string toEmail, string msg)
         {
-            var item = new Message
-            {
-                Id = Guid.NewGuid().ToString(),
-                FromEmail = fromEmail,
-                ToEmail = toEmail,
-                Msg = msg,
-                DateSend = DateTime.Today
-            };
-            db.messages.Add(item);
-            db.SaveChanges();
-            Clients.User("admin@gmail.com").SendMsgForAdmin();
+            MessageDb messageDb = new MessageDb();
+            var createDate = DateTime.Today;
+            messageDb.AddMessage(fromEmail, toEmail, msg, createDate);
+
+            var connectionId = Context.ConnectionId;
+
+            Clients.User("admin@gmail.com").SendMsgForAdmin(msg, createDate, connectionId);
 
 
         }
